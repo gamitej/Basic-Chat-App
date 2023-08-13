45f96a99a2c2fe76ee65c3c287d6c55e6b3066d6
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-13 12:49:09 +0530

    server setup done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User diconnected", socket.id);
+  });
+});
 
 app.listen(6000, () => {
   console.log("running");
