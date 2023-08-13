ff81cbc16e2ea10a93c3f602b66c08d994036481
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-13 14:14:10 +0530

    sad

--- server/index.js
+++ server/index.js
@@ -10,19 +10,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User connected to ${socket.id}`);
 
   socket.on("disconnect", () => {
     console.log("User diconnected", socket.id);
   });
 });
 
-app.listen(6000, () => {
+app.listen(3001, () => {
   console.log("running");
 });
